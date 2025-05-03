bc02403c5baf383ed3aca0d46e87308f19158d41
rahulyadav108 <ry1975908@gmail.com>
2025-05-03 19:15:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,20 +9,23 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyD4rUpC-Y7m1hGwKydLjTN4hnH4PUejF-0",
   authDomain: "paymentloo.firebaseapp.com",
   databaseURL: "https://paymentloo-default-rtdb.firebaseio.com",
   projectId: "paymentloo",
-  storageBucket: "paymentloo.firebasestorage.app",
+  storageBucket: "paymentloo.appspot.com",
   messagingSenderId: "678382986235",
   appId: "1:678382986235:web:14d671959c28ec800a215e",
   measurementId: "G-33XCH79XP2"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
 
 // Google Login Button
 const googleBtns = document.querySelectorAll(".google-login");
